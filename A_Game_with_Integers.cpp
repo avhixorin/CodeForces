2384012cf1d6eafcf62d5a40e7d4ec02c901b0d4
avhixorin <avhixorin@gmail.com>
2024-12-22 20:03:14 +0530

    Add solutions for A_Game_with_Integers, A_Jagged_Swaps, and B_Chemistry problems

---
+++ A_Game_with_Integers.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pi pair<long, long>
+#define vipi vector<pair<long, long>>
+#define vi vector<long long>
+#define pub push_back
+#define pob pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define umap unordered_map
+#define uset unordered_set
+#define pq priority_queue
+#define haa cout << "YES\n";
+#define nahi cout << "NO\n";
+#define cut cout << endl;
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return __gcd(a, b);
+}
+
+void avhixorin()
+{
+    int n;
+    cin >> n;
+    if (n % 3 != 0)
+        cout << "First" << endl;
+    else
+        cout << "Second" << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        avhixorin();
+    }
+
+    return 0;
+}
---
+++ A_Jagged_Swaps.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pi pair<long, long>
+#define vipi vector<pair<long, long>>
+#define vi vector<long long>
+#define pub push_back
+#define pob pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define umap unordered_map
+#define uset unordered_set
+#define pq priority_queue
+#define haa cout << "YES\n";
+#define nahi cout << "NO\n";
+#define cut cout << endl;
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return __gcd(a, b);
+}
+
+void avhixorin() {
+    int n;
+    cin >> n;
+    vi inp(n); 
+    for (auto &i : inp) cin >> i;
+    if(inp[0] == 1) haa
+    else nahi
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        avhixorin();
+    }
+
+    return 0;
+}
